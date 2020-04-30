d96ca85219e3eb6036bd331f73bb7bb8e14f77f5
MasatoMakino <unagiinu.g@gmail.com>
2020-04-30 17:30:44 +0900

    update : dependencies.

--- bin/CanvasParticleWay.d.ts
+++
@@ -1,6 +0,0 @@
-import { ParticleWay } from "particle-waypoint";
-export declare class CanvasParticleWay extends ParticleWay {
-    constructor(points: number[][], option?: {});
-    set points(points: number[][]);
-}
-//# sourceMappingURL=CanvasParticleWay.d.ts.map
--- gulpfile.ts
+++ gulpfile.ts
@@ -6,9 +6,11 @@ const server = require("gulptask-dev-server").get("./docs/demo");
 const { bundleDemo, watchDemo } = require("gulptask-demo-page").get({
   externalScripts: ["//code.createjs.com/1.0.0/tweenjs.min.js"],
   body: `<canvas id="appCanvas" width="640" height="480"></canvas>`,
-  style: `body{background-color: #000}`
+  style: `body{background-color: #000}`,
 });
-const { tsc, tscClean, watchTsc } = require("gulptask-tsc").get();
+const { tsc, tscClean, watchTsc } = require("gulptask-tsc").get({
+  projects: ["./tsconfig.json"],
+});
 
 const watchTasks = async () => {
   watchDemo();
